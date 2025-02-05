3d268a8b7b65353c970eb49aeef9b0e481951c20
MeetxDev <102921640+MeetxDev@users.noreply.github.com>
2025-02-05 19:55:03 +0530

    security fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import md5 from 'md5';
 import { createClient } from '@supabase/supabase-js';
 import bodyParser from 'body-parser';
 
@@ -15,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, 'public')));
 
 const supabaseUrl = 'https://mnhwkmcnammwycsiudku.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uaHdrbWNuYW1td3ljc2l1ZGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2OTAzNjEsImV4cCI6MjA1NDI2NjM2MX0.m-OgOXybV8AU0zHCJwYTm6B4rwcHqSHAtPnFZszlSEs";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
@@ -24,10 +23,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const { userid, password } = req.body;
-    if (!userid || !password) return res.status(400).send("Missing userid or password");
+    const { userid, passwordHash } = req.body;
+    if (!userid || !passwordHash) return res.status(400).send("Missing userid or password");
 
-    const password_hash = md5(password);
 
     const {data, error} = await supabase
         .from('users')
@@ -38,7 +36,7 @@ app.post('/login', async (req, res) => {
 
     if (error || !data) return res.send("No User Found");
 
-    if (data.password_hash != password_hash) return res.send("Incorrect Password");
+    if (data.password_hash != passwordHash) return res.send("Incorrect Password");
 
     if (data.role == 'basic') return res.json({ success: true, user: data });
 
