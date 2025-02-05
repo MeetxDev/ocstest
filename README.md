effe1a550837e77686d7f0b0b8aff2235d6ce262
MeetxDev <102921640+MeetxDev@users.noreply.github.com>
2025-02-05 20:04:50 +0530

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Recruitment task for OCS IIT Delhi
+
+Problem Statement:
+
+Develop an API endpoint and a frontend client to access and display data from the Table to the browser client. Keep the following things in mind:
+
+1) The code for the front end should NOT contain any sensitive information that could be accessed using the Developer console.
+2) All data exchange with the SQL table must occur on the server (Through the API endpoint).
+3) The password_hash field in the table is the md5 hash of the raw_password.
+4) The Browser Client must not transfer raw_password to the server.
+
+Used Node.js with express for api 
+supabase as server
+vercel for hosting 
--- app.js
+++ app.js
@@ -2,30 +2,32 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { createClient } from '@supabase/supabase-js';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Indroduce middleware to make our code clean
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(join(__dirname, 'public')));
 
-const supabaseUrl = 'https://mnhwkmcnammwycsiudku.supabase.co'
+// Connect Supabase server
+const supabaseUrl = 'https://mnhwkmcnammwycsiudku.supabase.co' 
 const supabaseKey = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uaHdrbWNuYW1td3ljc2l1ZGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2OTAzNjEsImV4cCI6MjA1NDI2NjM2MX0.m-OgOXybV8AU0zHCJwYTm6B4rwcHqSHAtPnFZszlSEs";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-
+// To redirect to index.html which takes user input
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'index.html'));
 });
 
+// Handles login request
 app.post('/login', async (req, res) => {
-    const { userid, passwordHash } = req.body;
+    const { userid, passwordHash } = req.body; // Gets Data from index.html
     if (!userid || !passwordHash) return res.status(400).send("Missing userid or password");
-
 
     const {data, error} = await supabase
         .from('users')
@@ -34,11 +36,11 @@ app.post('/login', async (req, res) => {
         .single();
 
 
-    if (error || !data) return res.send("No User Found");
+    if (error || !data) return res.send("No User Found"); // Checks if user with userid present
 
-    if (data.password_hash != passwordHash) return res.send("Incorrect Password");
+    if (data.password_hash != passwordHash) return res.send("Incorrect Password"); // Matches password hash
 
-    if (data.role == 'basic') return res.json({ success: true, user: data });
+    if (data.role == 'basic') return res.json({ success: true, user: data }); // Checks role
 
     const {data: allUsers, error: adminError} = await supabase
             .from('users')
